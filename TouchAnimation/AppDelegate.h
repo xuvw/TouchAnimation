57fa5556c9280ce6f8e76dc077b333927358740d
heke <heke@soyoung.com>
2016-01-29 12:44:35 +0800

    Initial Commit

---
+++ TouchAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchAnimation
+//
+//  Created by heke on 29/1/16.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchAnimation
+//
+//  Created by heke on 29/1/16.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
